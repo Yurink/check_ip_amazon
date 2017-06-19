74687b4db8cebc649ec7ff072f525206ffc3397f
Yurink <yurink1@gmail.com>
2017-06-19 14:35:16 +0200

    Add files via upload

--- check_ip_amazon.py
+++ check_ip_amazon.py
@@ -2,26 +2,29 @@
 import getopt, json, os.path, sys, urllib.request
 from urllib.request import urlopen
 from ipaddress import ip_network, ip_address
+from pathlib import Path
 
-# Ayuda
+# Mostrar Ayuda
 def usage():
-   print ('Usage: check_ip_amazon [-hd] [-ip Direccion IP]')
+   print ('Uso: check_ip_amazon [-h] [-d] [-i]')
    print
    print ('-h --help			Imprimir Ayuda')
    print ('-d				Descargar el fichero JSON de Amazon ip-ranges.json')
-   print ('-p				Amazon Host para comprobar')
+   print ('-i				Amazon Host para comprobar')
 
 # Descargar JSON data de Amazon
 def downloadJSONdata_ip_amazon():
-	#descargar la ultima version del fichero json con todas las ip's de amazon
+	#Descargar la ultima version del fichero json con todas las ip's de amazon
 	#crear el fichero ip-ranges.json, que se importara despues
-	print ("Descargand Json....")
+	print ("Descargando Json....")
 	url = "https://ip-ranges.amazonaws.com/ip-ranges.json"
 	outfile = "ip-ranges.json"
 	urllib.request.urlretrieve(url, outfile)
 
+ip_check = ""
+	
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hdi:", ["help","d","input"])
+    opts, args = getopt.getopt(sys.argv[1:], "hdi:", ["help","d"])
 except getopt.GetoptError as err:
     # print help information and exit
     print (str(err))
@@ -34,24 +37,24 @@ for o, a in opts:
        sys.exit()
     elif o in ('-d'):
         downloadJSONdata_ip_amazon()
-    elif o in ('-i', '--input'):
+    elif o in ('-i'):
         ip_check = a
     else:
          assert False, "Error parametro no valido"
 
+#Si no se define una ip para comprobar salimos
+if not ip_check:
+	sys.exit()
+
+file_ip_range = Path("ip-ranges.json")
+if not file_ip_range.is_file():
+    downloadJSONdata_ip_amazon()
+	
 data = json.load(open('ip-ranges.json'))
 for d in data["prefixes"]:
-	#print (d["ip_prefix"])
-	#aIpamazon.append(d["ip_prefix"])
 	net = ip_network(d["ip_prefix"])
-	#print(ip_address(ip_check) in net)
 	if ip_address(ip_check) in net:
 		print ("encontrado")
 		print ("La ip", ip_check , " pertenece al rango" , d["ip_prefix"])
-
-
-
-
-
 
 
